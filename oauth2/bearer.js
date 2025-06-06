e6aee18ba2a4ddd93d1fe696b1cd517f19b8f3ef
dfonsecam <dfonseca@impesa.net>
2025-06-06 17:03:33 -0600

    Add .env file for OAuth2 configuration with secret value

--- oauth2/bearer.js
+++ oauth2/bearer.js
@@ -1,21 +1,21 @@
 import jsonwebtoken from "jsonwebtoken";
 
 export function generateBearerToken(req, res) {
-    const user = req.user;
-    const secret = "my-super-secret-value";
-    const expiresIn = "1h";
+  const user = req.user;
+  const expiresIn = "1h";
+  const secret = process.env.SECRET;
 
-    const token = jsonwebtoken.sign(
-        {
-            sub: user.id,
-            name: user.name,
-        },
-        secret,
-        { expiresIn },
-    );
-    res.json({
-        access_token: token,
-        token_type: "Bearer",
-        expires_in: expiresIn,
-    });
+  const token = jsonwebtoken.sign(
+    {
+      sub: user.id,
+      name: user.name,
+    },
+    secret,
+    { expiresIn },
+  );
+  res.json({
+    access_token: token,
+    token_type: "Bearer",
+    expires_in: expiresIn,
+  });
 }
